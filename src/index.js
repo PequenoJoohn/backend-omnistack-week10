ac97067d7680c772b94e924c4b1bc476c5a90a17
Jonathan L. Matos <kheymk@hotmail.com>
2020-01-15 18:40:07 -0300

    Add files via upload
    Complete Backend Week OmniStack 10

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:7895123698@cluster0-fzhvj.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333, () => {
+    try {
+        console.log(`Server running port 3333.\nhttp://localhost:3333`)
+    } catch (error) {
+        console.log(error)
+    }
+});
+    // Métodos HTTP: get, post, put, delete
+
+    // Tipos de parâmetros: 
+    // Query Params: request.query (Filtros, ordenação, paginação)
+    // Route Params: request.params (Identificar um recurso na alteração ou remoção)
+    // Body: request.body (Dados para criação ou alteração de um registro)
+
+    // MongoDB (Não-relacional)
